cacfb341adcc44729f1b3a2be97a8e11e976403f
ChiLi-zy <1162995411@qq.com>
2020-09-03 21:56:44 +0800

    初始化项目并配置SpringBootActuator服务运行监控

---
+++ micservice-main/src/main/java/com/cloud/mic/micservicemain/MicserviceMainApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.mic.micservicemain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicserviceMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicserviceMainApplication.class, args);
+    }
+
+}
---
+++ micservice-main/src/test/java/com/cloud/mic/micservicemain/MicserviceMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.mic.micservicemain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicserviceMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

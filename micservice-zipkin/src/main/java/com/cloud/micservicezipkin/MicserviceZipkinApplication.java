c5ad69dd4a8a6aa4e369a08d39ea9bdb08806d30
ChiLi-zy <1162995411@qq.com>
2020-12-03 18:48:48 +0800

    4.7、添加swagger、链路追踪

---
+++ micservice-zipkin/src/main/java/com/cloud/micservicezipkin/MicserviceZipkinApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.micservicezipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class MicserviceZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicserviceZipkinApplication.class, args);
+    }
+
+}
---
+++ micservice-zipkin/src/test/java/com/cloud/micservicezipkin/MicserviceZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.micservicezipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicserviceZipkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
